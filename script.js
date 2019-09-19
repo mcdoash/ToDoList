0b05db79939549162e41ca65d8a27192324edd54
mcdoash <mcdoash@users.noreply.github.com>
2019-09-19 08:41:31 -0400

    Implement duration and sorting types

--- script.js
+++ script.js
@@ -7,14 +7,29 @@ document.getElementById("remove").addEventListener("click", clearItems);
 //event listener for highlight button
 document.getElementById("highlight").addEventListener("click", toggleHighlight);
 
-//event listener for sort button
-document.getElementById("sort").addEventListener("click", sortItems);
 
 
 //Adds the item entered by the user to the list
 function addItem() {
     //get value of item entered
     var itemName = document.forms["addItem"]["itemName"].value;
+    var hours = document.forms["addItem"]["hours"].value;
+    var minutes = document.forms["addItem"]["minutes"].value;
+    var displayTime = false;
+    
+    if(hours == "" && minutes == ""){
+        var totalTime = 0;
+    }
+    else {
+        if(hours == ""){
+            hours = 0;
+        }
+        else if(minutes == "") {
+            minutes = 0;
+        }
+        var totalTime = hours*60 + minutes;
+        displayTime = true;
+    }
     
     if(validItem(itemName)) {
         //clear form
@@ -23,7 +38,7 @@ function addItem() {
         //create new div element to hold all the new item elements
         var newItem = document.createElement("li");
         newItem.classList.add("item");
-        newItem.setAttribute("value", itemName);
+        newItem.setAttribute("time", totalTime);
         newItem.setAttribute("onClick", "checked(this)");
         
         //create heading for name
@@ -32,9 +47,27 @@ function addItem() {
         //add item name to heading
         var newItemText = document.createTextNode(itemName);
         newItemName.appendChild(newItemText);
-       
+        
         //add heading to div
         newItem.appendChild(newItemName);
+        
+        //create and add elements for time
+        if(displayTime) {
+            //create heading for time
+            var timeHeading = document.createElement("h2");
+
+            //add time to heading
+            var timeText = document.createTextNode( hours + "h " + minutes + "m");
+            timeHeading.appendChild(timeText);
+            
+            //add clock icon and time value
+            newItem.innerHTML += '<i class="far fa-clock fa-lg"></i>';
+        newItem.appendChild(timeHeading);
+        }
+        
+        
+        
+         
         
         //get list and add new item
         var list = document.getElementById("listItems");
@@ -82,8 +115,9 @@ function toggleHighlight() {
 }
 
 
-function sortItems() {
-    var swaping, items, swap;
+function sortItems(type, directon) {
+    
+    var swaping, items, swap, a, b;
     var list = document.getElementById("listItems");
     
     do {
@@ -92,7 +126,35 @@ function sortItems() {
         
         for(var i=0; i<(items.length-1); i++) {
             swap = false;
-            if(items[i].textContent > items[i+1].textContent) {
+            
+            //if first type (aplhanumerical)
+            if(type == 0) {
+                //if sorting ascending
+                if(directon == 0){
+                    a = items[i].textContent;
+                    b = items[i+1].textContent;
+                }
+                //sort descending
+                else {
+                    b = items[i].textContent;
+                    a = items[i+1].textContent;
+                }
+            }
+            //second type (duration)
+            else {
+                //if sorting ascending
+                if(directon == 0){
+                    a = items[i].getAttribute("time");
+                    b = items[i+1].getAttribute("time");
+                }
+                //sort descending
+                else {
+                    b = items[i].getAttribute("time");
+                    a = items[i+1].getAttribute("time");
+                }
+            }
+            
+            if(a > b) {
                 //swap items
                 items[i].parentNode.insertBefore(items[i+1], items[i]);
             swaping = true;
