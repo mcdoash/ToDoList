b4957469b15a86a8ca708dece62ae7666f8df9c6
mcdoash <mcdoash@users.noreply.github.com>
2019-09-24 12:27:34 -0400

    Comments and small style changes

--- script.js
+++ script.js
@@ -7,83 +7,91 @@ document.getElementById("remove").addEventListener("click", clearItems);
 //event listener for colour picker
 document.getElementById("colourPicker").addEventListener("change", colourChange);
 
-//Adds the item entered by the user to the list
+
+//addItem function adds the item entered by the user to the list
 function addItem() {
-    //get value of item entered
-    var itemName = document.forms["addItem"]["itemName"].value;
-    var colour = document.forms["addItem"]["colour"].value;
-    var hours = document.forms["addItem"]["hours"].value;
-    var minutes = document.forms["addItem"]["minutes"].value;
-    var displayTime = false;
+    //get value of item name entered
+    var name = document.forms["addItem"]["itemName"].value;
     
-    if(hours == "" && minutes == ""){
-        var totalTime = 0;
-    }
-    else {
-        if(hours == ""){
-            hours = 0;
+    //check if name if valid before continuing
+    if(validItem(name)) {
+        //get the remainder of item values
+        var colour = document.forms["addItem"]["colour"].value;
+        var hours = document.forms["addItem"]["hours"].value;
+        var minutes = document.forms["addItem"]["minutes"].value;
+
+        //determine if time should be displayed
+        var displayTime = false;
+        if(hours == "" && minutes == ""){ //both values empty
+            var totalTime = 0;
         }
-        else if(minutes == "") {
-            minutes = 0;
+        else {
+            if(hours == ""){
+                hours = 0;
+            }
+            else if(minutes == "") {
+                minutes = 0;
+            }
+            var totalTime = hours*60 + minutes; //create total time (in minutes) for sorting later on
+            displayTime = true;
         }
-        var totalTime = hours*60 + minutes;
-        displayTime = true;
-    }
-    
-    if(validItem(itemName)) {
+        
         //clear form
         document.getElementById("addItem").reset();
-        colourChange();
+        colourChange(); //reset colour picker icon colour
         
         //create new div element to hold all the new item elements
         var newItem = document.createElement("li");
         newItem.classList.add("item");
-        newItem.setAttribute("time", totalTime);
+        newItem.setAttribute("time", totalTime); //set values entered for easy sorting later
         newItem.setAttribute("colour", colour);
-        newItem.setAttribute("onClick", "checked(this)");
+        newItem.addEventListener("click", checked(this)); //add event listener to check off item when clicked
         
-        //create heading for name
-        var newItemName = document.createElement("h2");
         
+        var nameHeading = document.createElement("h2"); //create heading for name
         //add item name to heading
-        var newItemText = document.createTextNode(itemName);
-        newItemName.appendChild(newItemText);
+        var nameText = document.createTextNode(name);
+        nameHeading.appendChild(nameText);
         
-        //add heading to div
-        newItem.appendChild(newItemName);
+        newItem.appendChild(nameHeading); //add heading to div
         
-        //create and add elements for time
+        //create and add elements for time, if needed
         if(displayTime) {
-            //create heading for time
-            var timeHeading = document.createElement("h2");
+            var timeHeading = document.createElement("h2"); //create heading for time
 
             //add time to heading
             var timeText = document.createTextNode( hours + "h " + minutes + "m");
             timeHeading.appendChild(timeText);
             
-            //add clock icon and time value
+            //add clock icon
             newItem.innerHTML += '<i class="far fa-clock fa-lg"></i>';
-        newItem.appendChild(timeHeading);
+            newItem.appendChild(timeHeading);
         }
         
         //change colour
-        newItem.style.color = colour;
-        newItem.firstChild.style.color = colour;
-        newItem.style.borderBottom = "2px solid " + colour;
+        newItem.style.color = colour; //icon colour
+        newItem.firstChild.style.color = colour; //text colour
+        newItem.style.borderBottom = "2px solid " + colour; //border colour
         
         //get list and add new item
         var list = document.getElementById("listItems");
         list.appendChild(newItem);
     }
+    //item name entered was not valid
     else {
         //display error box
-        alert("Please add a valid item");
+        alert("Please add a valid item.");
     }
 }
 
-//checks if an item name is valid
-function validItem(itemName) {
-    if(itemName != "") {
+
+/*
+validItem function checks if an item name is valid
+@param      name    text to be checked for validity 
+@return             boolean if input is valid
+*/
+function validItem(name) {
+    if(name != "") { //not empty
         return true;
     }
     else {
@@ -91,30 +99,47 @@ function validItem(itemName) {
     }
 }
 
+
+/*
+checked function toggles item's checked state via a class
+@param      item    item whose checked state is to be altered
+*/
 function checked(item) {
     item.classList.toggle("checked");
 }
 
-//clears all checked items from the list
+
+/*
+clearItems function clears all checked items from the list
+*/
 function clearItems() {
-        //create list of all divs with class "checked"
+    //get all checked items
     var checkedItems = document.getElementsByClassName("checked");
     
-    //remove every div with a checked box
+    //remove every item that has been checked
     while(checkedItems[0]) {
         checkedItems[0].parentNode.removeChild(checkedItems[0]);
     }
 }
 
-
+/*
+function sortItems sorts list contents based on arguments
+@param      type        type of sort to be conducted. 
+                        0: alphanumericly
+                        1: by duration
+                        2: by colour
+            direction   direction of sort
+                        0: ascending
+                        1: descending
+*/
 function sortItems(type, directon) {
-    
     var swaping, items, swap, a, b;
-    var list = document.getElementById("listItems");
+    var list = document.getElementById("listItems"); //list element
     
+    //simple bubble sort 
     do {
         swaping = false;
-        items = list.getElementsByTagName("li");
+        items = list.getElementsByTagName("li"); //get new list of items (ordering will change)
         
         for(var i=0; i<(items.length-1); i++) {
             swap = false;
@@ -145,25 +170,27 @@ function sortItems(type, directon) {
                     a = items[i+1].getAttribute("time");
                 }
             }
+            //third type (colour)
             else {
                 a = items[i].getAttribute("colour");
                     b = items[i+1].getAttribute("colour");
             }
             
             if(a > b) {
-                //swap items
-                items[i].parentNode.insertBefore(items[i+1], items[i]);
-            swaping = true;
+                items[i].parentNode.insertBefore(items[i+1], items[i]); //swap items
+                swaping = true; //continue to sort
                 break;
             }
         }
-        
     } while(swaping);
 }
 
 
+/*
+colourChange function changes the colour of the colour pick icon based on used entry
+This is to overwride the styling of the innate colour input element
+*/
 function colourChange() {
     var colourPicker = document.getElementById("colourPicker");
-    
-    colourPicker.parentElement.style.color = colourPicker.value;
+    colourPicker.parentElement.style.color = colourPicker.value; //set colour of icon to value of colour picker
 }
